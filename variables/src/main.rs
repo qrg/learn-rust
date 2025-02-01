1a33f1538d08021c789824dfbee6641b28b744e0
qrg <qualle.rhythmus+d@gmail.com>
2025-02-02 02:19:30 +0900

    Shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,11 @@
-#![allow(unused)]
 fn main() {
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    let x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
 }
