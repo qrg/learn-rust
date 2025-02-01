63f3545e2239daf68b67d9de30b2036d12248df8
qrg <qualle.rhythmus+d@gmail.com>
2025-02-02 02:19:07 +0900

    Constants

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,4 @@
+#![allow(unused)]
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 }
