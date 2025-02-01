2d2af148fa40ebb115be8a861b98e194b71406c9
qrg <qualle.rhythmus+d@gmail.com>
2025-02-01 23:05:10 +0900

    Comparing the Guess to the Secret Number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -15,6 +16,14 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 
     println!("You guessed: {}", guess);
 }
